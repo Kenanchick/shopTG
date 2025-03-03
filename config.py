35b6bfe3b9149ca02bf593f66de76e345e8bb1ee
Kenanchick <galandarovkenan057@gmail.com>
2025-03-03 22:29:29 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN='7931577079:AAHTbOpxn13J6P2lSyqj2kXYTWbU1oEr2Uw'
+DB_URL='sqlite+aiosqlite:///db.sqlite3'
+#DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
+
+# Внимание! Добавьте config.py в файл .gitignore!
